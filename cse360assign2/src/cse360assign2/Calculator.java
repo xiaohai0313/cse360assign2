13f45f2021b59cfb9970f7ea03375ca9138663bf
Xueling <xlmeng@asu.edu>
2019-02-24 16:10:53 -0700

    Initial version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,43 @@
+package cse360assign2;
+/**
+ * This class implements a simple calculator structure, calculator could do 
+ * add, subtract, multiply and add operation, display calculated result.
+ * 
+ * 
+ * @author Xueling Meng
+ * @ClassID: 489
+ * @since 2019-2-24
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
